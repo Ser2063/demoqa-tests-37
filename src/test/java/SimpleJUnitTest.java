ed6485fd6d75484f62e367f480b600ca4f08c8ab
Ser2063 <jca@mail.ru>
2025-08-18 00:00:14 +0300

    added JUnit

---
+++ src/test/java/SimpleJUnitTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class SimpleJUnitTest {
+
+    @Test
+    void firstTest() {
+        System.out.println("###    firstTest()");
+        Assertions.assertTrue(3 > 2);
+    }
+}
