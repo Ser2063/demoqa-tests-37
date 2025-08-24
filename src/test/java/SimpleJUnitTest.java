b9747ce893939480b6386be61b6ba3473f8f7b0b
Ser2063 <jca@mail.ru>
2025-08-24 16:29:11 +0300

    added JUnit

--- src/test/java/SimpleJUnitTest.java
+++ src/test/java/SimpleJUnitTest.java
@@ -1,5 +1,4 @@
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
@@ -7,9 +6,52 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class SimpleJUnitTest {
 
+    int result;
+
+    @BeforeAll
+    static void beforeAll(){
+        System.out.println("\n### beforeAll()\n");
+    }
+
+
+    @AfterAll
+    static void afterAll(){
+        System.out.println("\n### afterAll()\n");
+    }
+
+    @BeforeEach
+    void beforeEach(){
+        System.out.println("###  beforeEach()");
+        result = getResult();
+
+    }
+
+    @AfterEach
+    void afterEach(){
+        System.out.println("###       afterEach()\n");
+        result = 0;
+
+    }
+
     @Test
     void firstTest() {
         System.out.println("###    firstTest()");
-        Assertions.assertTrue(3 > 2);
+        Assertions.assertTrue(result > 2);
+    }
+
+    @Test
+    void secondTest() {
+        System.out.println("###    firstTest()");
+        Assertions.assertTrue(result > 2);
+    }
+
+    @Test
+    void thirdTest() {
+        System.out.println("###    firstTest()");
+        Assertions.assertTrue(result > 2);
+    }
+
+    private int getResult() {
+        return 3;
     }
 }
